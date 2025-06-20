63f16021ea06d5a27cafe0fbc992697a4cf5db33
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-06-20 16:18:58 -0300

    feat: verify location

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -2,24 +2,29 @@
 import { Api } from "@/services/api";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { toast } from "sonner";
 
 export function useAuth() {
+  const location = useLocation();
+
   const query = useQuery({
     queryKey: ["auth-user"],
     queryFn: async () => {
       const { data } = await Api.get("/auth/profile");
-      console.log(data)
+
       return data
     },
     retry: false
   });
 
   useEffect(() => {
-    if (query.isError) {
-      toast.success("Unauthorized!");
+    const isOnAuthPage = location.pathname === "/sign-in";
+
+    if (query.isError && !isOnAuthPage) {
+      toast.error("Unauthorized!");
     }
-  }, [query.isError]);
+  }, [query.isError, location]);
 
   return query
 }
