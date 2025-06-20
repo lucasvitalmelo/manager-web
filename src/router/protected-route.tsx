925b98331722b2b6e0208c7ca36674d25fbdf63d
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-06-20 16:21:27 -0300

    feat: LoadCircle icon

--- src/router/protected-route.tsx
+++ src/router/protected-route.tsx
@@ -1,11 +1,16 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "@/hooks/useAuth";
+import { useAuth } from "@/hooks/use-auth";
+import { LoaderCircle } from "lucide-react";
 
 export function ProtectedRoute() {
   const { data: user, isLoading, isError } = useAuth();
 
   if (isLoading) {
-    return <div>Carregando...</div>;
+    return (
+      <div className="w-full flex justify-center items-center mt-16">
+        <LoaderCircle className="animate-spin" size={20} />
+      </div>
+    );
   }
 
   if (!user || isError) {
